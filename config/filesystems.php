43313e90a82cc2e3d3c0d456faa3775f30fe3874
escettitt <tekillermd@gmail.com>
2020-03-21 12:45:18 +0300

    url

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('app/public'),
+            'url' => 'https://desolate-waters-59345.herokuapp.com'.'/storage',
             'visibility' => 'public',
         ],
 
